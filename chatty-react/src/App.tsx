4a85fceedd827456dd52f2ee463d69d06d7c302d
LONGHI BENJAMIN ALEJO <47955882@ort>
2024-05-30 12:04:30 -0300

    cooking

--- chatty-react/src/App.tsx
+++ chatty-react/src/App.tsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const aparecerChatty = async () => {
+  let [tab] = await chrome.tabs.query({ active: true })
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id! },
+    func: () => {
+      const chatty = document.createElement('img')
+      chatty.src = './public/chatty.png'
+      chatty.style.position = 'fixed'
+      chatty.style.bottom = '0'
+      chatty.style.right = '0'
+      chatty.style.width = '300px'
+      chatty.style.height = '300px'
+      chatty.style.zIndex = '9999'
+      document.body.appendChild(chatty)
+    }
+  })
+}
 
+function App() {
   return (
     <>
       <div className='flex flex-row justify-around pl-3 pr-4'>
@@ -18,8 +33,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={aparecerChatty}>
+          Aparecer Chatty
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
