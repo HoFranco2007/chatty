46b9288ababaaf6d84e05d5479843ed09667b589
Benja Longhi <benjaminlonghi10@gmail.com>
2024-05-31 11:48:06 -0300

    Canvas

--- chatty-react/components/ChattyCanvas.tsx
+++ chatty-react/components/ChattyCanvas.tsx
@@ -1,11 +1,16 @@
 import { Canvas } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
+import { Suspense } from 'react';
 
-const ChattyCanvas= () => {
+const ChattyCanvas = () => {
   const chatty = useGLTF("/lion/chatty.gltf");
+
   return (
-    <Canvas frameloop="demand" camera={{ position: [-4, 3, 6], fov: 45, near: 0.1, far: 200 }}> 
-      <primitive object={chatty.scene} scale={0.5} />    
+    <Canvas frameloop="demand" camera={{ position: [-4, 3, 6], fov: 45, near: 0.1, far: 200 }}>
+      <Suspense fallback={null}>
+        <primitive object={chatty.scene} scale={0.5} />
+        <ambientLight intensity={0.5} />
+      </Suspense>
     </Canvas>
   );
 };
