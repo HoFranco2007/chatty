33ac7f7135c92bd3b316462aa06d0728d950bd9b
Benja Longhi <benjaminlonghi10@gmail.com>
2024-06-10 17:23:12 -0300

    Update Background.js

--- chatty-react/public/background.js
+++ chatty-react/public/background.js
@@ -1,3 +1,6 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 const checkPosition = () => {
   chrome.storage.local.get(['positionBottom', 'positionLeft'], ({ positionBottom, positionLeft }) => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -5,7 +8,7 @@ const checkPosition = () => {
         chrome.scripting.executeScript({
           target: { tabId: tabs[0].id },
           func: (positionBottom, positionLeft) => {
-            const chatty = document.getElementById('chatty-img');
+            const chatty = document.getElementById('chatty');
             if (chatty) {
               chatty.style.bottom = `${positionBottom}px`;
               chatty.style.left = `${positionLeft}px`;
@@ -18,18 +21,43 @@ const checkPosition = () => {
                 document.body.style.backgroundColor = 'rgba(0, 0, 0, 0)';
               }
             } else {
-              const newChatty = document.createElement('img');
-              newChatty.className = `transition-all duration-300 ease-in-out `
-              newChatty.id = 'chatty-img'
-              newChatty.src = chrome.runtime.getURL("/chatty.png");
-              newChatty.style.position = 'absolute';
-              newChatty.style.bottom = `${positionBottom}px`;
-              newChatty.style.left = `${positionLeft}px`;
-              newChatty.style.width = '300px';
-              newChatty.style.height = '300px';
-              newChatty.style.zIndex = '9999';
-              newChatty.style.opacity = '1';
-              document.body.appendChild(newChatty);
+                const scene = new THREE.Scene();
+                const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+                const renderer = new THREE.WebGLRenderer();
+                renderer.setSize( window.innerWidth, window.innerHeight );
+                renderer.setAnimationLoop( animate );
+                renderer.domElement.id = "chatty"
+                document.body.appendChild( renderer.domElement );
+
+                const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+                const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+                const newChatty = new THREE.Mesh( geometry, material );
+                scene.add( newChatty );
+
+                camera.position.z = 5;
+
+                function animate() {
+
+                  newChatty.rotation.x += 0.01;
+                  newChatty.rotation.y += 0.01;
+
+                  renderer.render( scene, camera );
+
+                }
+
+                render.setAnimationLoop(animate)
+
+              // const newChatty = document.createElement('div');
+              // newChatty.className = `transition-all duration-300 ease-in-out `
+              // newChatty.style.position = 'absolute';
+              // newChatty.style.bottom = `${positionBottom}px`;
+              // newChatty.style.left = `${positionLeft}px`;
+              // newChatty.style.width = '300px';
+              // newChatty.style.height = '300px';
+              // newChatty.style.zIndex = '9999';
+              // newChatty.style.opacity = '1';
+              // document.body.appendChild(newChatty);
             }
           },
           args: [positionBottom, positionLeft]
