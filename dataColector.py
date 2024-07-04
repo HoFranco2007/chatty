d441c35cafe37092c030b31744eabcbb9706aa4c
HoFranco2007 <127147452+HoFranco2007@users.noreply.github.com>
2024-07-04 11:57:52 -0300

    correcciones en el codigo

--- dataColector.py
+++ dataColector.py
@@ -1,12 +1,10 @@
 import subprocess
-from urllib3.exceptions import InsecureRequestWarning
-import csv
-import pandas as pd
-from sklearn.model_selection import train_test_split
+
 import requests
+
 from urllib3.exceptions import InsecureRequestWarning
 
-""" librerias = ['beautifulsoup4', 'torch', 'transformers', 'scikit-learn', 'lxml', 'transformers[torch] accelerate -Ufff', 'google.generativeai']
+""" librerias = ['beautifulsoup4', 'requests', 'urllib3', 'pandas', 'lxml', 'google.generativeai']
 for libreria in librerias:
     try:
         subprocess.check_call(['pip', 'install', libreria])
@@ -15,12 +13,13 @@ for libreria in librerias:
         print(f"Error al instalar la librería {libreria}.")
         
         # Deshabilitar las advertencias de solicitud HTTP insegura
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning) """
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)  """
 
-from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
-from transformers import pipeline
 from bs4 import BeautifulSoup
-import torch
+import csv
+import pandas as pd
+
+
 
 
 def get_html_from_url(url):
@@ -88,7 +87,6 @@ with open(csv_filename, mode='w', newline='', encoding='utf-8') as csv_file:
     for clave, contenido in clas_elementos.items():
         writer.writerow({'clave': clave, 'contenido': contenido})
 
-print(html_content)
 
 # Cargar el archivo CSV
 csv_filename = "elementos_extraccion.csv"
@@ -121,59 +119,3 @@ data = parse_html(html_content)
 
 # Convertir las etiquetas a enteros
 data['label'] = data['label'].astype('category').cat.codes
-
-""" # Dividir los datos en conjuntos de entrenamiento y validación
-train_texts, val_texts, train_labels, val_labels = train_test_split(data['text'], data['label'], test_size=0.4, random_state=42)
-
-# Tokenizar los textos
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-train_encodings = tokenizer(list(train_texts), truncation=True, padding=True)
-val_encodings = tokenizer(list(val_texts), truncation=True, padding=True)
-
-# Definir la clase del dataset
-class WebElementsDataset(torch.utils.data.Dataset):
-    def __init__(self, encodings, labels):
-        self.encodings = encodings
-        self.labels = labels
-
-    def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        item['labels'] = torch.tensor(self.labels[idx])
-        return item
-
-    def __len__(self):
-        return len(self.labels)
-
-# Crear los datasets de entrenamiento y validación
-train_dataset = WebElementsDataset(train_encodings, list(train_labels))
-val_dataset = WebElementsDataset(val_encodings, list(val_labels))
-
-# Cargar el modelo BERT preentrenado
-model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=data['label'].nunique())
-
-# Configurar los argumentos de entrenamiento
-training_args = TrainingArguments(
-    output_dir='./results',
-    num_train_epochs=3,
-    per_device_train_batch_size=8,  # Disminuir el tamaño del batch
-    per_device_eval_batch_size=16,  # Disminuir el tamaño del batch
-    warmup_steps=500,
-    weight_decay=0.01,
-    logging_dir='./logs',
-    logging_steps=10,
-)
-
-# Crear el entrenador
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_dataset,
-    eval_dataset=val_dataset,
-)
-
-# Entrenar el modelo
-trainer.train()
-
-# Guardar el modelo y el tokenizador entrenados
-model.save_pretrained('./web_element_classifier')
-tokenizer.save_pretrained('./web_element_classifier') """
--- ia.py
+++ ia.py
@@ -1,23 +1,23 @@
 import os
 import google.generativeai as genai
 
-import AI_prueba as iA
+import dataColector as dt
 
 genai.configure(api_key="AIzaSyBzdj7Fi4I7mxwPCg-QvVtfdX93vRgpaFI")
 
 
-data_content = iA.data
+data_content = dt.data
 # Assuming 'data_content' is a DataFrame and you want to convert it to a list of strings
 content_list = data_content.astype(str).values.flatten().tolist()
 
 index = 1
 textoref = "usando el codigo html proporcionado antes"
-textodef = "pasar solamente el paso numero 1 ya estando en la pagina para saber "
-texto = "como accedo a la informacion de alberto sileoni en esta pagina"
+textodef = "pasar solamente el paso numero 1 ya estando en la pagina para saber ESPECIFICAMENTE EL SELECTOR UNICO QUE DIFERENCIE AL OBJETO"
+
+texto = "como inicio session en esta pagina ?"
+
 
 textodef = textoref + textodef + texto
-
-print(textodef)
 
 inputContent = [item + textodef for item in content_list]
 
@@ -27,4 +27,6 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 
 # Generar contenido
 response = model.generate_content(inputContent)
+
+
 print(response.text)
