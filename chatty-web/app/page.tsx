cefe771063bde37b168e59be979eba40dc4bf7ef
Benja Longhi <benjaminlonghi10@gmail.com>
2024-08-01 14:30:39 -0300

    Update Landing Page

--- chatty-web/app/page.tsx
+++ chatty-web/app/page.tsx
@@ -9,17 +9,13 @@ export default async function Home() {
   return (
     <body className="">
       <section className="bg-black h-[100vh] w-[100vw] grid-background">
+        <div className="absolute h-[100vh] w-[100vw]">
+            <img src="/bg.png" alt="" className="relative h-[100vh] w-[100vw] -top-12 -left-12"/>
+        </div>
         <Navbar isOpen={false} page={"Home"} />
           <div className="flex flex-row justify-around">
-            <div className="absolute h-[100vh] w-[100vw]">
-              <img src="/background.png" alt="" className="absolute h-[100vh] w-[100vw] -top-12"/>
-            </div>
             <div className="flex flex-col justify-around">
               <div className="flex justify-start flex-col h-[20vw]">
-                <span className="absolute mx-auto py-[0.15vw] flex border w-fit blur-xl bg-[#85F900] bg-clip-text text-[8.5vw] box-content font-extrabold text-transparent text-center select-none">
-                    CHATTY
-                </span>
-                <p className="flex items-baseline text-[8.5vw] font-extrabold text-[#85F900] h-[10vw]">CHATTY</p>
                 <p className="flex ml-2 items-end text-[4vw] font-bold mt-4 h-[5vw] bg-gradient-to-r from-[#5B0662] to-[#F580F1] text-transparent bg-clip-text">THE WEB HELPER</p>
                 <div>
                   <p className="text-[1.3vw] font-bold text-[#CCCCCC] w-[35vw] mt-[3vw] ml-[1vw]">
@@ -28,7 +24,7 @@ export default async function Home() {
                 </div>
               </div>
               <div className="flex items-center ">
-                <button className="bg-gradient-to-r from-[#85F900] to-[#85F900]/70 p-[1vw] text-[1.3vw] text-black font-extrabold rounded-3xl mt-[3vw] ml-[1vw]">ADD TO CHROME</button>
+                <button className="bg-gradient-to-r from-[#85F900] to-[#85F900]/70 p-[1vw] text-[1.3vw] text-black font-extrabold rounded-3xl mt-[2vw] ml-[1vw]">ADD TO CHROME</button>
               </div>
             </div>
             <div>
--- chatty-web/components/Navbar.tsx
+++ chatty-web/components/Navbar.tsx
@@ -12,7 +12,7 @@ import { useRouter } from "next/navigation";
 
 const Navbar = ({isOpen, page} : {isOpen : boolean; page : string}) => {
   const [scrolled, setScrolled] = useState(false);
-  const [smallScreen, setSmallScreen] = useState(0);
+  const [smallScreen, setSmallScreen] = useState();
   const [hoveredIndex, setHoveredIndex] = useState(0);
 
   const router = useRouter()
@@ -39,64 +39,8 @@ const Navbar = ({isOpen, page} : {isOpen : boolean; page : string}) => {
   };
   return (
     <main className="top-0 overflow-hidden pb-36">
-        {smallScreen ? (
-          <section className="w-full h-full">
-            <nav className={`fixed flex flex-row items-center justify-center w-full mb-32 ${ scrolled || isOpen ? "bg-black border-b border-b-white transition-all duration-1000 shadow-4xl" : "bg-black transition-all duration-1000 border-b-0 border-b-white/0"}`}>
-              <div className="ml-6">
-                <button
-                  className="bg-none border-0 cursor-pointer w-[60px] h-[60px] p-0"
-                >
-                  <span
-                    className={`rounded-[5px] bg-white/70 block w-[40%] mt-[5px] mb-[auto] h-[2px] transition-all duration-700 ${
-                      isOpen ? "rotate-45 translate-y-[7px]" : ""
-                    }`}
-                  />
-                  <span
-                    className={`rounded-[5px] block w-[40%] mt-[5px] mb-[auto] h-[2px] transition-all ${
-                      isOpen
-                        ? "bg-none bg-transparent"
-                        : "bg-white/70 duration-1000"
-                    }`}
-                  />
-                  <span
-                    className={`rounded-[5px] bg-white/70 block w-[40%] mt-[5px] mb-[auto] h-[2px] transition-all duration-700 ${
-                      isOpen ? "-rotate-45 -translate-y-[7px]" : ""
-                    }`}
-                  />
-                </button>
-              </div>
-              <div className="flex flex-row items-center justify-center mr-12 w-full">
-                <p className="" >
-                    <img src="/logo.png" alt="logo" className="p-4 w-20" />
-                </p>
-                {/* <div>
-                  <SearchBar />
-                </div> */}
-              </div>
-            </nav>
-            <div className={`fixed top-[5.7rem] w-[14rem] h-full bg-black transition-all duration-1000 ${isOpen ? "border-r border-r-white transition-all duration-1000" : "border-r-0 border-r-white/0 transition-all duration-1000 -translate-x-[14rem]"}`}>
-              <ul className="list-none flex flex-col items-center" id="nav-list">
-                <li className={`m-4 text-base cursor-pointer px-3 py-2 rounded-full transition-colors duration-200 ${page === "Home" ? "text-white bg-[#272727]" : "text-white/70 hover:text-white hover:bg-[#0f0f0f]"}`}> Home </li>
-                <li className={`m-4 text-base cursor-pointer px-3 py-2 rounded-full transition-colors duration-200 ${page === "Best Sellers" ? "text-white bg-[#272727]" : "text-white/70 hover:text-white hover:bg-[#0f0f0f]"}`}> Best Sellers </li>
-                <li className={`m-4 text-base cursor-pointer px-3 py-2 rounded-full transition-colors duration-200 ${page === "Cuidado Corporal" ? "text-white bg-[#272727]" : "text-white/70 hover:text-white hover:bg-[#0f0f0f]"}`}> Cuidado Corporal </li>
-                <li className={`m-4 text-base cursor-pointer px-3 py-2 rounded-full transition-colors duration-200 ${page === "Cuidado Facial" ? "text-white bg-[#272727]" : "text-white/70 hover:text-white hover:bg-[#0f0f0f]"}`}> Cuidado Facial </li>
-                <li className={`m-4 text-base cursor-pointer px-3 py-2 rounded-full transition-colors duration-200 ${page === "Cuidado Capilar" ? "text-white bg-[#272727]" : "text-white/70 hover:text-white hover:bg-[#0f0f0f]"}`}> Cuidado Capilar </li>
-                <li className={`m-4 text-base cursor-pointer px-3 py-2 rounded-full transition-colors duration-200 ${page === "Cuidado de Manos" ? "text-white bg-[#272727]" : "text-white/70 hover:text-white hover:bg-[#0f0f0f]"}`}> Cuidado de Manos </li>
-                </ul>
-                <div>
-                  <ul className="flex flex-col items-center justify-center mt-40 w-full">
-                    <li className="mb-2 xl:mr-2"><p className="flex flex-col items-center justify-center text-black font-semibold text-base px-2 py-2 bg-white/90 rounded-md cursor-pointer hover:bg-white"> Contactanos </p></li>
-                    <li className="mt-2 xl:ml-2"><p className="flex flex-col items-center justify-center text-white/70 font-semibold text-base px-4 py-2 bg-[#1d1c1c56] rounded-md border border-[#525050] cursor-pointer hover:bg-[#1a1919]"> Nosotros </p></li>
-                  </ul>
-                </div>
-            </div>
-          </section>
-        ) : (
-          <header className={`fixed flex flex-row items-center justify-around w-full mb-32 ${ scrolled ? "bg-none border-b border-b-white transition-all duration-1000 shadow-4xl" : "bg-black transition-all duration-1000 border-b-0 border-b-white/0"}`}>
-            <nav className="flex flex-row items-center justify-around w-full ">
-              <p className="navbar-brand d-flex items-center text-white" >
-                  <img src="/chatty.png" alt="logo" className="p-4 w-20" />
-              </p>
+          <header className={`fixed flex flex-row items-center justify-around w-full mb-32 ${ scrolled ? "bg-none border-b border-b-white transition-all duration-1000 shadow-4xl" : " transition-all duration-1000 border-b-0 border-b-white/0"}`}>
+            <nav className="flex flex-row items-center justify-between w-full px-8">
               <ul className="list-none flex flex-row items-center" id="nav-list">
               {Array.from({ length: 4 }, (_, index) => (
                 <li
@@ -137,7 +81,6 @@ const Navbar = ({isOpen, page} : {isOpen : boolean; page : string}) => {
               </div>
             </nav>
           </header>
-        )}
     </main>
   )}  
 
