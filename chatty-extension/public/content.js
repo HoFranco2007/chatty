090f9cfc1a93b46ffa97e64f8ed6faf7c30bce1d
Benja Longhi <benjaminlonghi10@gmail.com>
2024-08-22 14:31:08 -0300

    Extension

--- chatty-extension/public/content.js
+++ chatty-extension/public/content.js
@@ -1,17 +1,3 @@
-// import React from 'react';
-// import { createRoot } from 'react-dom/client';
-// import App from './App'
-
-// const body = document.querySelector('body')
-// const app = document.createElement('div')
-
-// app.id = 'react-root'
-
-// if (body) {
-//   body.prepend(app)
-// }
-
-// const container = document.getElementById('react-root');
-// const root = createRoot(container);
-
-// root.render(<App/>)
+const html = document.documentElement.outerHTML;
+console.log(html);
+chrome.runtime.sendMessage({ action: "captureHTML", data: html });
