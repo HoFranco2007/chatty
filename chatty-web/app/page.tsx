564afdf7b00d1e5c23d175a4c65f11b448adff16
Tomas Rodriguez Moyano <110465901+Toto20087@users.noreply.github.com>
2024-08-28 19:14:51 -0300

    correcciones

--- chatty-web/app/page.tsx
+++ chatty-web/app/page.tsx
@@ -1,8 +1,30 @@
 import Navbar from "../components/navbar/navbar";
 import Footer from "../components/footer/footer"
 import "./globals.css";
+import Card from "@/components/card";
+import SFCAdvantages from "@/components/info";
+
+
+const DiscoverIcon = () => (
+  <svg className="w-8 h-8 text-purple-400" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M8 16l4-4m0 0l4-4m-4 4l-4-4m4 4l4 4" />
+  </svg>
+);
+
+const DecisionIcon = () => (
+  <svg className="w-8 h-8 text-purple-400" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4v16m8-8H4" />
+  </svg>
+);
+
+const TimeIcon = () => (
+  <svg className="w-8 h-8 text-purple-400" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 8v4l3 3m6-5a9 9 0 11-18 0 9 9 0 0118 0z" />
+  </svg>
+);
 
 export default async function Home() {
+
   return (
     <>
       <section className="absolute">
@@ -38,6 +60,32 @@ export default async function Home() {
             </aside>
           </section>
         </main>
+        <div className="flex flex-row mt-10">
+          <Card
+          title="Descubre Información Oculta"
+          description="Deja que nuestra IA explore la web por ti, encontrando datos valiosos que podrían haberte pasado por alto. Haz que la información trabaje a tu favor."
+          linkText="Descubre Más"
+          linkUrl="/home"
+          icon={<DiscoverIcon />}
+          />
+          <Card
+            title="Toma Decisiones Más Inteligentes"
+            description="Convierte los datos en decisiones. Nuestra IA te ofrece análisis detallados para que tomes decisiones más informadas y estratégicas."
+            linkText="Comienza Ahora"
+            linkUrl="/home"
+            icon={<DecisionIcon />}
+          />
+          <Card
+            title="Ahorra Tiempo y Recursos"
+            description="Automatiza la recopilación y análisis de datos, ahorrando tiempo y recursos valiosos. Enfócate en lo que realmente importa, nosotros nos encargamos del resto."
+            linkText="Optimiza Ahora"
+            linkUrl="/home"
+            icon={<TimeIcon />}
+          />
+      </div>
+      <section className="flex items-center justify-cente flex-col mt-10">
+        <SFCAdvantages />
+      </section>
         <footer className="flex justify-center w-[100vw]">
           <Footer />
         </footer>
