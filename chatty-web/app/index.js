ef6be2c5466cd053fad3c48d2f7f6db5d0a364d2
Tomas Rodriguez Moyano <110465901+Toto20087@users.noreply.github.com>
2024-09-30 15:21:16 -0300

    conexion va mejorando

--- chatty-web/app/index.js
+++ chatty-web/app/index.js
@@ -10,7 +10,7 @@ app.use(cors({
   }));
   
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use("/", router);
 
--- chatty-web/routes/selector.js
+++ chatty-web/routes/selector.js
@@ -6,7 +6,6 @@ export const supabaseClient = createBrowserClient(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNlZ3dwYXVlZ3hkcXlmb2x2cXJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYyMjQxMzQsImV4cCI6MjAzMTgwMDEzNH0.8V-VAfb2983I8RkqEPHc5t7YyxbbBFbbjZb2eCnEGRE")
 const router = Router();
 
-// Define your routes here
 router.get('/data', (req, res) => {
     res.json({ message: 'Hello from /data endpoint!' });
 });
@@ -14,26 +13,24 @@ router.get('/data', (req, res) => {
 router.post('/getData', async (req, res) => {
     const receivedData = req.body;
     console.log('Received data:', receivedData);
+    const contentId = undefined;
 
     try {
-        // Buscar si el dato ya existe en la tabla
         const { data: existingData, error: fetchError } = await supabaseClient
             .from('content')
             .select('*')
             .eq('content', receivedData)
-            .single(); // Asumiendo que solo debería haber un resultado
+            .single();
 
         if (fetchError) {
             console.error('Error fetching data:', fetchError);
             return res.status(500).json({ message: 'Error checking existing data', error: fetchError });
         }
 
-        // Si el dato ya existe, enviar una respuesta indicando que ya existe
         if (existingData) {
             return res.status(409).json({ message: 'Data already exists', data: existingData });
         }
 
-        // Si el dato no existe, realizar la inserción
         const { data, error: insertError } = await supabaseClient
             .from('content')
             .insert({"content": receivedData});
@@ -43,17 +40,37 @@ router.post('/getData', async (req, res) => {
             return res.status(500).json({ message: 'Failed to insert data', error: insertError });
         }
 
-        // Envía una respuesta confirmando la inserción
         res.json({ message: 'Data received and inserted successfully', data });
+
+        contentId = data[0].id;
     } catch (error) {
         console.error('Unexpected error:', error);
         res.status(500).json({ message: 'Unexpected error', error });
     }
+
+    router.post("/getUrl", async (req, res) => {
+        const receivedUrl = req.body;
+        console.log('Received data:', receivedUrl);
+
+        const { data: webData, error: webError } = await supabase
+        .from('webs')
+        .insert([
+        { url: receivedUrl, content_id: contentId }
+        ]);
+
+        if (webError) {
+            console.error('Error al insertar URL:', webError);
+            return;
+        }
+
+        console.log('Datos insertados con éxito:', webData);
+    })
 });
+
+
 
 router.get('/getDataFromDB', async (req, res) => {
     try {
-        // Recupera el último dato insertado en la tabla "content"
         const { data, error } = await supabaseClient
             .from('content')
             .select('*')
@@ -69,7 +86,6 @@ router.get('/getDataFromDB', async (req, res) => {
             return res.status(404).json({ message: 'No data found in the content table' });
         }
 
-        // Envía el último registro recuperado como respuesta
         res.json(data[0]);
     } catch (error) {
         console.error('Unexpected error:', error);
